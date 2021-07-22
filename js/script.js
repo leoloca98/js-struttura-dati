64668b163fff556d4cbda9e5f1e89a0c795e70eb
leoloca98 <leonardolocatelli98@gmail.com>
2021-07-22 17:20:23 +0200

    Add Display Stamp

--- js/script.js
+++ js/script.js
@@ -13,7 +13,7 @@ attenzione a dettagli grafici (va bene una lista coi tag UL e LI)
 cardTemplate = document.getElementById("card-template");
 
 const card = {
-    id=1,
+    id: 1,
     nameCard: "Grizzly Bears",
     launchCost: ["1", "G"],
     convertedManaCost: 2,
@@ -28,9 +28,51 @@ const card = {
         rarity: "Commmon",
         cardNbr: 268
     },
-    collectionNbr: "268/301",
+    abilities: [],
     strenghtCard: 2,
     costitutionCard: 2,
-    artist: "D. J. Cleland-Hura",
+    artist: "D. J. Cleland-Hura"
+};
 
+//CONTROLLI PER IL TEMPLATE
+let subType = card.optionalType ? ` -  ${card.optionalType}` : "";
+let abilitiesMessage = `<i>Nessuna abilità</i>`;
+if (card.abilities.length) {
+    abilitiesMessage = `<ul>`;
+    for (let i = 0; i < card.abilities.length; i++) {
+        let currentAbility = card.abilities[i];
+        abilitiesMessage += `<li>Description: ${currentAbility.description}</li>`
+        abilitiesMessage += `<li>Costo di lancio: ${currentAbility.launchCost}</li>`
+    }
+    abilitiesMessage += `</ul>`;
 }
+
+//CREAZIONE DEL TEMPLATE
+let cardDisplay = `
+<ul class="card-type">
+    <li>Id: ${card.id}</li>
+    <li>Name: ${card.nameCard}</li>
+    <li>Launch Cost: ${card.launchCost}</li>
+    <li>Converted Mana Cost: ${card.convertedManaCost}</li>
+    <li>Card type: ${card.cardType} ${subType}</li>
+    <li>Expansion: 
+        <ul>
+            <li> Name Expansion: ${card.nameExpansion}</li>
+            <li> Rarity: ${card.rarity}</li>
+            <li> Card Number: ${card.cardNbr}</li>
+        </ul>
+    </li>
+    <li>Flavor Text:
+        <ul>
+            <li>Text: ${card.text}</li>
+            <li>Author: ${card.author}</li>
+        </ul>
+    </li>
+    <li>Abilities: ${abilitiesMessage}</li>    
+    <li>Strength: ${card.strenghtCard}</li>    
+    <li>Costitution: ${card.costitutionCard}</li>
+    <li>Artist: ${card.artist}</li>    
+</ul>`;
+
+
+cardTemplate.innerHTML = cardDisplay;
